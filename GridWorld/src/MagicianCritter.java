b7accc282706ec69b211605078793bf745f9988d
apcragg <apcragg@gmail.com>
2014-02-26 12:39:28 -0500

    magic
    Magic

--- GridWorld/src/MagicianCritter.java
+++ GridWorld/src/MagicianCritter.java
@@ -3,7 +3,6 @@ import info.gridworld.actor.Bug;
 import info.gridworld.actor.Critter;
 import info.gridworld.actor.Flower;
 import info.gridworld.actor.Rock;
-import info.gridworld.grid.Grid;
 import info.gridworld.grid.Location;
 
 import java.awt.Color;
@@ -29,31 +28,31 @@ public class MagicianCritter extends Critter
 			if(a instanceof Bug)
 			{
 				Data<Actor> d = new Data<Actor>(a);
-				a = d.resetActor(a.getGrid());
+				a = d.resetActor(Actor.class);
 			}
 			
 			else if(a instanceof Critter && !(a instanceof MagicianCritter))
 			{
 				Data<Bug> d = new Data<Bug>(a);
-				a = d.resetActor(getGrid());
+				a = d.resetActor(Bug.class);
 			}
 			
 			else if(a instanceof Rock)
 			{
 				Data<Flower> d = new Data<Flower>(a);
-				a = d.resetActor(getGrid());
+				a = d.resetActor(Flower.class);
 			}
 			
 			else if(a instanceof Flower)
 			{
 				Data<Rock> d = new Data<Rock>(a);
-				a = d.resetActor(getGrid());
+				a = d.resetActor(Rock.class);
 			}
 			
 			else
 			{
 				Data<Critter> d = new Data<Critter>(a);
-				a = d.resetActor(getGrid());
+				a = d.resetActor(Critter.class);
 			}
 		}
 	}
@@ -73,18 +72,17 @@ public class MagicianCritter extends Critter
 			a.removeSelfFromGrid();
 		}
 		
-		public T resetActor(Grid<Actor> g)
-		{			
-			Class<T> cl = null;
-			
+		public T resetActor(Class<T> cl)
+		{	
 			T a = null;
+			
 			try { a = cl.newInstance(); }
 			catch (Exception e) {}
 
 			a.setColor(c);
 			a.setDirection(d);
 			
-			a.putSelfInGrid(g, l);
+			a.putSelfInGrid(getGrid(), l);
 			
 			return a;
 		}
